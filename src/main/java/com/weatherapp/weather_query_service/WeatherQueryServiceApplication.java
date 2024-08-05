aa3f0bde94fcb8fb02dd75da6a89bf25ab21baaf
kokilaw <kokilapc@gmail.com>
2024-08-06 01:03:27 +1000

    task(): initial commit

---
+++ src/main/java/com/weatherapp/weather_query_service/WeatherQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.weatherapp.weather_query_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherQueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherQueryServiceApplication.class, args);
+	}
+
+}
