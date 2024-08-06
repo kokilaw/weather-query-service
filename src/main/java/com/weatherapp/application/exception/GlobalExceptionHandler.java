c50de9d1a094555a9022ae25dd9564366c2183f4
kokilaw <kokilapc@gmail.com>
2024-08-07 03:28:50 +1000

    task(): logging errors

--- src/main/java/com/weatherapp/application/exception/GlobalExceptionHandler.java
+++ src/main/java/com/weatherapp/application/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.weatherapp.application.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,25 +11,30 @@ import java.util.Collections;
 import java.util.Optional;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ApiKeyNotFoundException.class)
     public ProblemDetail handleApiKeyNotFoundError(ApiKeyNotFoundException ex) {
+        log.error("Error occurred - ", ex);
         return getProblemDetail(HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(ExpiredApiKeyException.class)
     public ProblemDetail handleKeyExpiredError(ExpiredApiKeyException ex) {
+        log.error("Error occurred - ", ex);
         return getProblemDetail(HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(ApiKeyRateExceededException.class)
     public ProblemDetail handleRateExceededError(ApiKeyRateExceededException ex) {
+        log.error("Error occurred - ", ex);
         return getProblemDetail(HttpStatus.TOO_MANY_REQUESTS);
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ProblemDetail handleNotFoundException(NotFoundException ex) {
+        log.error("Error occurred - ", ex);
         ProblemDetail problemDetail = getProblemDetail(HttpStatus.NOT_FOUND, ex.getMessage());
         Optional.ofNullable(ex.getResourceIdMap()).orElseGet(Collections::emptyMap)
                 .forEach(problemDetail::setProperty);
@@ -37,6 +43,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(InvalidParameterException.class)
     public ProblemDetail handleInvalidParameterException(InvalidParameterException ex) {
+        log.error("Error occurred - ", ex);
         ProblemDetail problemDetail = getProblemDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
         Optional.ofNullable(ex.getResourceIdMap()).orElseGet(Collections::emptyMap)
                 .forEach(problemDetail::setProperty);
@@ -45,11 +52,13 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(InternalServerError.class)
     public ProblemDetail handleInternalServerError(InternalServerError ex) {
+        log.error("Error occurred - ", ex);
         return getProblemDetail(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(Exception.class)
     public ProblemDetail handleGeneralException(Exception ex) {
+        log.error("Error occurred - ", ex);
         return getProblemDetail(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
